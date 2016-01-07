3a17432f1c9ccf1071300067171b92af0f5aa3ca
L3viathan <git@l3vi.de>
2016-01-07 23:58:58 +0100

    some more names

--- src/jsx/SuffixList.jsx
+++ src/jsx/SuffixList.jsx
@@ -1,16 +1,3 @@
-const suffixList = [
-            ["a"],
-            ["ad"],
-            ["ani"],
-            ["ea"],
-            ["ești"],
-            ["ila"],
-            ["iu"],
-            ["oara"],
-            ["ov", "ova"],
-            ["ui"],
-            ["și","ș"],
-            ["ța","ți"]
-	];
+const suffixList = [['ți','ța','țu'],['oieni'],['oi','oaia'],['ău','aua'],['ov','ova'],['ceni'],['tari'],['ani','eni'],['easa','easca','oasa','osu','oasei'],['ma'],['ele'],['uc'],['ica'],['iș','uș'],['anu'],['oara'],['ești'],['ila'],['iu'],['ași','aș','așu'],['uca','ica'],['che','chea'],['lung','lungă'],['escu'],['eia', 'ea'],['dia']];
 
 export default suffixList;
